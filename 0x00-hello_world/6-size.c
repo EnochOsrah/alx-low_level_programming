ea1897e22ac01a51ae8cdaa0ac8817872f9a07da
EnochOsrah <xuccessbwoygh@gmail.com>
2023-07-13 16:39:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-/*Description:"This function prints the size of various types."*/
-printf("CHAR: %lu\n", sizeof(char));
-printf("SHORT: %lu\n", sizeof(short));
-printf("INT: %lu\n", sizeof(int));
-printf("LONG: %lu\n", sizeof(long));
-printf("LONG LONG: %lu\n", sizeof(long long));
-printf("FLOAT: %lu\n", sizeof(float));
-printf("DOUBLE: %lu\n", sizeof(double));
-printf("LONG DOUBLE: %lu\n", sizeof(long double));
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of short: %zu bytes\n", sizeof(short));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of long double: %zu bytes\n", sizeof(long double));
 return (0);
 }
