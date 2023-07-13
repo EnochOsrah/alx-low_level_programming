575e9a74fc42338e49fd811ff101885ecd101407
EnochOsrah <xuccessbwoygh@gmail.com>
2023-07-13 15:14:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main = Entry point
- *
+ * Description: prints "\"Programming is like building a multilingual puzzle
  * Retrun: Always 0 (Success)
  */
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
