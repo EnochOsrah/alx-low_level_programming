54395728a520b9ed8e30a9420d2434c5d72d9be3
EnochOsrah <xuccessbwoygh@gmail.com>
2023-07-18 14:12:40 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,13 @@
-#include "main.h"
-
-/**
- * main - Print _putchar
- * Return: Always 0 (succces)
- */
-
-int main(void)
-{
-	char ch[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-		_putchar(ch[i]);
-	_putchar('\n');
-	return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
