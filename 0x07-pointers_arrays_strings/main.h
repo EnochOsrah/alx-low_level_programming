35797ea6370fdd9cd961fda9c6973c0e79a5b60b
EnochOsrah <xuccessbwoygh@gmail.com>
2023-08-02 13:25:47 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Isaac Annor
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
-
 #include <stddef.h>
 
 int _putchar(char c);
