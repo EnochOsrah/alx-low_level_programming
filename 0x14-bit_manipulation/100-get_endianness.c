c86a1d90030e4c8abff94531ebcb584fb412818d
EnochOsrah <xuccessbwoygh@gmail.com>
2023-08-31 12:42:35 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 | 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	return (*c == 1);
+}
