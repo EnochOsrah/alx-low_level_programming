04b917e3cb2b43bdabe40604cbe47ee596a28639
EnochOsrah <xuccessbwoygh@gmail.com>
2023-09-04 13:22:34 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,23 +13,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written = 0;
+	int fd, len, write_status;
 
 	if (filename == NULL)
-
 	return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	return (-1);
 
 	if (text_content != NULL)
-	while (*text_content != '\0')
-	bytes_written = write(fd, text_content, 1);
-	if (bytes_written == -1)
+	{
+	len = 0;
+	while (text_content[len])
+	len++;
+
+	write_status = write(fd, text_content, len);
+	if (write_status == -1)
+	{
 	close(fd);
 	return (-1);
-	text_content++
+	}
+	}
 
 	close(fd);
 	return (1);
